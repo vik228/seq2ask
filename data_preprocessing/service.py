44691e4402f095e54d66db784b9a5a9f1dcc8115
vikas pandey <vikaspandey@vikass-MacBook-Pro.local>
2024-02-09 01:44:01 +0530

    fixed service

--- data_preprocessing/service.py
+++ data_preprocessing/service.py
@@ -1,14 +1,28 @@
+from __future__ import annotations
+
+
 class Service:
+
     def __init__(self, data_loader, data_preprocessor, mode='train') -> None:
         self.data_loader = data_loader
         self.data_preprocessor = data_preprocessor
         self.mode = mode
-    
+
     def load_data(self):
         self.data_loader.download_squad()
         filename = self.data_loader.train_file if self.mode == 'train' else self.data_loader.dev_file
         return self.data_loader.load_squad_data(filename)
 
-    def prepare_squad_training_input(self):
+    def prepare_squad_training_input(self, combine_context_and_questions=False):
         contexts, questions, answers = self.load_data()
-        return self.data_preprocessor.preprocess(contexts), self.data_preprocessor.preprocess(questions), self.data_preprocessor.preprocess(answers)
+        if combine_context_and_questions:
+            inputs = [
+                context + self.data_preprocessor.context_question_seperator +
+                question for context, question in zip(contexts, questions)
+            ]
+            input_sequence = self.data_preprocessor.preprocess(inputs)
+            output_sequence = self.data_preprocessor.preprocess(answers)
+            return input_sequence, output_sequence
+        return self.data_preprocessor.preprocess(
+            contexts), self.data_preprocessor.preprocess(
+                questions), self.data_preprocessor.preprocess(answers)

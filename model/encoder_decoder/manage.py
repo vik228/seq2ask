ebf72b389a4c0a756d739ddf2af9a411e5e2adb9
vikas pandey <vikaspandey@vikass-MacBook-Pro.local>
2024-03-24 01:11:54 +0530

    model and training params uploaded to gcs

--- model/encoder_decoder/manage.py
+++ model/encoder_decoder/manage.py
@@ -2,13 +2,17 @@ from __future__ import annotations
 
 import argparse
 import json
+import logging
+from pathlib import Path
 
 import numpy as np
 from data_generator import DataGenerator
+from env import bucket_name
 from env import bucket_path_model
 from env import set_env_vars
 from optimizer import Optimizer
 from sklearn.model_selection import train_test_split
+from utils import download_blob
 
 if __name__ == '__main__':
     set_env_vars()
@@ -18,8 +22,8 @@ if __name__ == '__main__':
         ('--input_seq_path', 'Path to input sequence csv file.'),
         ('--decoder_input_path', 'Path to decoder input sequence csv file.'),
         ('--decoder_output_path', 'Path to decoder output sequence csv file.'),
-        ('--model_params', 'Path to model params.'),
-        ('--training_params', 'Path to training params.'),
+        ('--model_params_bucket_path', 'Path to model params.'),
+        ('--training_params_bucket_path', 'Path to training params.'),
     ]
     for arg in cl_args:
         parser.add_argument(
@@ -48,8 +52,21 @@ if __name__ == '__main__':
                                     y_decoder_target_padded_train)
     val_generator = DataGenerator(X_val, y_decoder_input_padded_val,
                                   y_decoder_target_padded_val)
-    model_params = json.loads(args.model_params)
-    training_params = json.loads(args.training_params)
+    Path.mkdir("data", exist_ok=True)
+    download_blob(
+        bucket_name=bucket_name,
+        source_blob_name=args.model_params_bucket_path,
+        destination_file_name="data/model_params.json",
+    )
+    download_blob(
+        bucket_name=bucket_name,
+        source_blob_name=args.training_params_bucket_path,
+        destination_file_name="data/training_params.json",
+    )
+    model_params = json.load(open("data/model_params.json"))
+    training_params = json.load(open("data/training_params.json"))
+    logging.info("Model params are %s" % model_params)
+    logging.info("Training params are %s" % training_params)
     training_params['train_generator'] = train_generator
     training_params['validation_generator'] = val_generator
     print("Training Params are %s" % training_params)
